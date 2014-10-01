1c1bd653de6fe3d133c1d30e1f15063c98687f9a
prathamalag1994 <prathamalag@berkeley.edu>
2014-09-30 17:28:00 -0700

    all

---
+++ script.rb
@@ -0,0 +1,11 @@
+     #!/usr/bin/env ruby
+     # Example application to demonstrate some basic Ruby features
+     # This code loads a given file into an associated application
+
+     require 'gibbon'
+     require 'mailgun'
+      class Launcher
+      	
+      end
+
+     launcher = Launcher.new
