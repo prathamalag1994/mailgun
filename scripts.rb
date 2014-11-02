1183e371c6a3f7bf454390bac78c2c3158572d37
prathamalag1994 <prathamalag@berkeley.edu>
2014-11-02 11:07:06 -0800

    back

--- scripts.rb
+++ scripts.rb
@@ -1,6 +1,3 @@
-     #!/usr/bin/env ruby
-     # Example application to demonstrate some basic Ruby features
-     # This code loads a given file into an associated application
 
      require 'gibbon'
      require 'mailgun'
